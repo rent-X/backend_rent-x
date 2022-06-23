ae0d89d113d707ddfdb6fc977e1633819ab55ad7
byronkamal <byronkamal@hotmail.com>
2022-06-23 01:44:04 -0300

    Create Category model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
+
+  categories.push(category);
 
   return response.status(201).json({ message: "Ok", categories });
 });
