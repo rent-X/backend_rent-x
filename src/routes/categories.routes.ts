8df29ec7856bf848ebcff29fb4ed8bab9c31ebf3
byronkamal <byronkamal@hotmail.com>
2022-12-01 02:08:56 -0300

    Create Category's repository dir and making some updates

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoryRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.status(201).json({ message: "Ok", categories });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
